5204eb4b8de0c4cd402e9dcc85e28e0e867ac753
wingsico <wingsico@outlook.com>
2017-04-16 12:12:09 +0800

    update

--- imformation.js
+++ imformation.js
@@ -1,14 +1,21 @@
-new Vue({
+var vm = new Vue({
   el: '#body',
   data: {
     message: '综合要闻',
     newsShow: true,
     surveyShow: false,
     noticeShow: false,
-    lists: {},
+    newsLists: {},
+    surveyLists: {},
+    noticeLists: {},
     showA: false,
     article: {},
-    loading: false
+    loading: false,
+    tips: '已获取最新列表',
+    showTips: false,
+    newsRead: [],
+    surveyRead: [],
+    noticeRead: []
   },
   methods: {
     ajax: function (method, url, cb, data, dataType) {
@@ -36,7 +43,6 @@ new Vue({
             console.log('请求错误:' + xhr.status) // 跳转至错误页面。可以自己定义
           }
           if (xhr.status === 200) {
-
             cb(xhr.responseText)
           }
         }
@@ -49,40 +55,117 @@ new Vue({
       this.noticeShow = false
       this.showA = false
       this.message = '综合要闻'
-      this.ajax('get', 'newsData.json', this.getNews)
+
     },
+
     surveyStyle: function () {
       this.newsShow = false
       this.surveyShow = true
       this.noticeShow = false
       this.showA = false
       this.message = '科学调研'
-      this.ajax('get', 'surveyData.json', this.getNews)
     },
+
     noticeStyle: function () {
       this.newsShow = false
       this.surveyShow = false
       this.noticeShow = true
       this.showA = false
       this.message = '教务通知'
-      this.ajax('get', 'noticeData.json', this.getNews)
+
     },
+
     getNews: function (data) {
       var jsonData = JSON.parse(data)
-      this.lists = jsonData
+      this.newsLists = jsonData
+      this.showTips = true
+      setTimeout(function () {
+        vm.showTips = false
+      }, 1200)
     },
-    getArticle: function (article) {
+
+    getSurvey: function (data) {
+      var jsonData = JSON.parse(data)
+      this.surveyLists = jsonData
+      this.showTips = true
+      setTimeout(function () {
+        vm.showTips = false
+      }, 1200)
+    },
+
+    getNotice: function (data) {
+      var jsonData = JSON.parse(data)
+      this.noticeLists = jsonData
+      this.showTips = true
+      setTimeout(function () {
+        vm.showTips = false
+      }, 1200)
+    },
+
+    setReadItems: function (name, nameId,itemName) {
+      name = JSON.parse(name)
+      if (name.length === 0) {
+        name.push(nameId)
+        var Read = JSON.stringify(name)
+        localStorage.setItem(itemName, Read)
+      } else {
+        for (var i = 0; i < name.length; i++) {
+          if (name[i] == nameId)
+            console.log('重复！')
+          else {
+            name.push(nameId)
+            var Read = JSON.stringify(name)
+            localStorage.setItem(itemName, Read)          }
+        }
+      }
+    },
+
+    getNewsArticle: function (article) {
       console.log(article.title)
       var articleId = article["news_id"]
-      this.ajax('get', articleId + '.json', this.showArticle)
+      var newsRead = localStorage.getItem('newsRead')
+      this.setReadItems(newsRead, articleId,'newsRead')
+      this.ajax('get', '01.json', this.showArticle)
+    },
+
+    getSurveyArticle: function (article) {
+      console.log(article.title)
+      var articleId = article["news_id"]
+      var surveyRead = localStorage.getItem('surveyRead')
+      surveyRead = JSON.parse(surveyRead)
+
+    },
+
+    getNoticeArticle: function (article) {
+      console.log(article.title)
+      var articleId = article["news_id"]
+      var noticeRead = localStorage.getItem('noticeRead')
+      noticeRead = JSON.parse(noticeRead)
+      for (var i = 0; i < noticeRead.length; i++) {
+        if (noticeRead[i] === articleId)
+          return false
+        else
+          noticeRead.push(articleId)
+      }
+      this.ajax('get', '/TeachingNotice/' + articleId, this.showArticle)
     },
     showArticle: function (data) {
       var jsonObj = JSON.parse(data)
       this.article = jsonObj
       this.showA = true
-    }
+    },
+
+    backToIndex: function () {
+      this.showA = !this.showA
+    },
+
   },
   mounted: function () {
-    this.newsStyle()
+    this.ajax('get', 'newsData.json', this.getNews)
+    this.ajax('get', 'surveyData.json', this.getSurvey)
+    this.ajax('get', 'noticeData.json', this.getNotice)
+    localStorage.setItem('newsRead', '[]')
+    localStorage.setItem('surveyRead', '[]')
+    localStorage.setItem('noticeRead', '[]')
   }
 })
